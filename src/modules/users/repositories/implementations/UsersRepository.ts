8211729e98f1cc5b99e7bb03da299309792427f0
degui1 <gui.denez56@gmail.com>
2024-01-10 09:21:31 -0300

    findUserByGameId

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -14,17 +14,27 @@ export class UsersRepository implements IUsersRepository {
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+    const x  = await this.repository.findOneOrFail(user_id, {
+      relations: ["games"]
+    });
+    console.log(x);
+    return x;
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query("SELECT * FROM users ORDER BY users.first_name");
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(
+      "SELECT * FROM users WHERE lower(users.first_name) = lower($1) AND lower(users.last_name) = lower($2)",
+      [
+        `${first_name}`,
+        `${last_name}`
+      ]
+    )
   }
 }
